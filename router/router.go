d6d943245fd2d2846c36e2f52788bd205f7c31fb
ko tainen <TonyK@hyvesolutions.com>
2020-03-04 22:58:28 +0800

    Add router.group

--- router/router.go
+++ router/router.go
@@ -1,11 +1,16 @@
 package router
+
 import (
 	"github.com/LiveShop/controllers"
 	"github.com/gin-gonic/gin"
 )
-func InitRouter() *gin.Engine{
-	router:=gin.Default()
-	router.GET("/",hello)
-	router.POST("/register",controllers.Register)
+
+func InitRouter() *gin.Engine {
+	router := gin.Default()
+	router.GET("/", hello)
+	v1 := router.Group("/v1")
+	{
+		v1.POST("/register", controllers.Register)
+	}
 	return router
 }

8df6c99561ff33dd57d5775ea99d854dc1e168a7
ko tainen <TonyK@hyvesolutions.com>
2020-03-05 14:03:50 +0800

    add query str of QueryUserWithParam

--- databases/queries.go
+++ databases/queries.go
@@ -3,4 +3,5 @@ package databases
 const (
 	IsUserExist = `SELECT id FROM users WHERE email=$1;`
 	CreateUserQuery = `INSERT INTO users(id,name,password,email) VALUES (DEFAULT, $1 , $2, $3);`
+	QueryUserWithParam = `WHERE username=$1 and password=$2`
 )

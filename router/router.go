47b81c4acde54b0c7fce7aa0244eb9718cbe9f9c
ko tainen <TonyK@hyvesolutions.com>
2020-03-06 02:18:44 +0800

    add authmiddleware

--- router/router.go
+++ router/router.go
@@ -2,16 +2,22 @@ package router
 
 import (
 	"github.com/LiveShop/controllers"
+	"github.com/LiveShop/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRouter() *gin.Engine {
 	router := gin.Default()
-	router.GET("/", hello)
+
 	v1 := router.Group("/v1")
 	{
 		v1.POST("/register", controllers.RegisterPost)
-		v1.POST("/login",controllers.LoginPost)
+		v1.POST("/login", middlewares.AuthMiddleware(middlewares.AllUserAuthorizator).LoginHandler)
+	}
+	auth := router.Group("/auth")
+	auth.Use(middlewares.AuthMiddleware(middlewares.AllUserAuthorizator).MiddlewareFunc())
+	{
+		auth.GET("/hello", hello)
 	}
 	return router
 }
